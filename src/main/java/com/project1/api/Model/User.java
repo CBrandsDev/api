895f60f6a3777375bc0efd9599c8b78081274c4c
Calil Brandão <cbrandao2001@gmail.com>
2024-05-07 20:16:16 -0300

    so much anotations

--- src/main/java/com/project1/api/Model/User.java
+++ src/main/java/com/project1/api/Model/User.java
@@ -1,7 +1,18 @@
 package com.project1.api.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Users")
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int code;
     private String name;
     private int age;
 
@@ -11,11 +22,17 @@ public class User {
     public int getAge() {
         return age;
     }
+    public int getCode() {
+        return age;
+    }
     public void setName(String name) {
         this.name = name;
     }
     public void setAge(int age) {
         this.age = age;
+    }
+    public void setCode(int code) {
+        this.code = code;
     }
 
 }
