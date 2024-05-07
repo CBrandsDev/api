99c2ad59c60280a8e67b9b892ab87f810d324382
Calil Brandão <cbrandao2001@gmail.com>
2024-05-07 19:25:50 -0300

    Testing some things

--- src/main/java/com/project1/api/Control/Control.java
+++ src/main/java/com/project1/api/Control/Control.java
@@ -1,14 +1,28 @@
 package com.project1.api.Control;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class Control {
-    
+
     @GetMapping("")
+    public String ok() {
+        return "save friends";
+    }
+    
+    @GetMapping("/welcome")
     public String testing() {
         return "Hi fellas";
     }
+
+    @GetMapping("/welcome/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Welcome " + name;
+    }
+    
 
 }
