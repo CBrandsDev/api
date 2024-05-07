10af442e4dd1ed49eddbd3d7f6cca5fc299e6d41
Calil Brandão <cbrandao2001@gmail.com>
2024-05-07 19:45:57 -0300

    lets lets

--- src/main/java/com/project1/api/Control/Control.java
+++ src/main/java/com/project1/api/Control/Control.java
@@ -2,7 +2,12 @@ package com.project1.api.Control;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project1.api.Model.User;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -25,4 +30,8 @@ public class Control {
     }
     
 
+    @PostMapping("/User")
+    public User user(@RequestBody User u) {
+        return u;
+    }
 }
